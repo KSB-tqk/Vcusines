1340a721fea385a73df6623178c27ba1f52a6e9a
KSB-tqk <linh05602@gmail.com>
2021-06-29 17:53:24 +0700

    commit

---
+++ lib/screens/login/authentication/login_details.dart
@@ -0,0 +1,239 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vcusines/components/constants.dart';
+import 'package:vcusines/notifier/authentication.dart';
+import 'package:provider/provider.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class LoginDetail extends StatefulWidget {
+  const LoginDetail({key}) : super(key: key);
+
+  @override
+  _LoginDetailState createState() => _LoginDetailState();
+}
+
+class _LoginDetailState extends State<LoginDetail> {
+  TextEditingController emailController;
+  TextEditingController passwordController;
+  TextEditingController resetPasswordMailController;
+  final _formKey = GlobalKey<FormState>();
+  String _email;
+  final auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final loginProvider = Provider.of<AuthenticationService>(context);
+    bool checkSnackBarShow = false;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            // Nhập tài khoản email
+            TextFormField(
+              controller: emailController,
+              textInputAction: TextInputAction.next,
+              validator: (val) =>
+              val.isNotEmpty ? null : "Email không được để trống!",
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.mail_rounded,
+                  color: Colors.black38,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black38),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: kPrimaryColor),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black38),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                contentPadding: EdgeInsets.all(10),
+                hintText: "Email",
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black26,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            // Nhập mật khẩu
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              inputFormatters: [FilteringTextInputFormatter.deny(new RegExp('[\\.|\\,| -]'))],
+              validator: (val) =>
+              val.length < 6 ? "Mật khẩu phải lớn hơn 6 kí tự!" : null,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.lock_rounded,
+                  color: Colors.black38,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black38),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: kPrimaryColor),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black38),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                contentPadding: EdgeInsets.all(10),
+                hintText: "Mật khẩu",
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black26,
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Đăng nhập ngay!",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black54,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Cùng trải nghiệm ẩm thực Việt!",
+              style: TextStyle(
+                fontSize: 14,
+                color: kPrimaryColor,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 200,
+              height: 50,
+              child: OutlinedButton(
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {}
+                  final String messege = await loginProvider.signIn(
+                    email: emailController.text.trim(),
+                    password: passwordController.text.trim(),
+                  );
+                  if (!checkSnackBarShow) {
+                    checkSnackBarShow = true;
+                    await ScaffoldMessenger.of(context)
+                        .showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          '$messege',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        backgroundColor: kPrimaryColor,
+                        duration: const Duration(milliseconds: 2000),
+                      ),
+                    )
+                        .closed;
+                    checkSnackBarShow = false;
+                  }
+                },
+                child: Text(
+                  "Đăng nhập",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+                style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    backgroundColor: kPrimaryColor),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+                onPressed: () {
+                  Alert(
+                      context: context,
+                      title: "Quên Mật Khẩu",
+                      content: Column(
+                        children: <Widget>[
+                          TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.mail, color: kPrimaryColor),
+                              labelText: 'Email',
+                              hintText: 'Nhập email bạn đã đăng ký',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: kPrimaryColor),
+                              ),
+                              focusColor: kPrimaryColor,
+                              fillColor: kPrimaryColor,
+                              hoverColor: kPrimaryColor,
+                            ),
+                            controller: resetPasswordMailController,
+                            onChanged: (value) {
+                              setState(() {
+                                _email = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      buttons: [
+                        DialogButton(
+                          onPressed: () {
+                            loginProvider.sendResetPassWord(_email);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(
+                            "Gửi Email Reset Mật Khẩu",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          color: kPrimaryColor,
+                        )
+                      ]).show();
+                },
+                child: Text("Quên Mật Khẩu?"),
+                style: TextButton.styleFrom(primary: Colors.black54))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/search_screen/search_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:vcusines/components/constants.dart';
+import 'package:vcusines/notifier/food_notifier.dart';
+import 'package:vcusines/components/food.dart';
+import 'package:vcusines/screens/home_screens/components_homescreen/food_item.dart';
+import 'package:provider/provider.dart';
+
+class SearchScreen extends StatefulWidget {
+  static String id = 'SearchScreen';
+  const SearchScreen({key}) : super(key: key);
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchTEC = new TextEditingController();
+  List<Food> foodListFound = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text("Kho ẩm thực".toUpperCase(),
+              style:
+              TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold)),
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+                    child: ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: Provider.of<FoodNotifier>(context)
+                            .findFoodList
+                            .length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          final i = Provider.of<FoodNotifier>(context)
+                              .findFoodList[index];
+                          if (index == 0) {
+                            return Padding(
+                              padding: EdgeInsets.only(
+                                top: 90,
+                              ),
+                              child: FoodItem(food: i),
+                            );
+                          }
+                          return FoodItem(food: i);
+                        }),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              decoration: BoxDecoration(
+                color: kPrimaryColor,
+                border: Border.all(color: kPrimaryColor),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                        controller: searchTEC,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            hintText: "Tìm kiếm món ăn theo tên.",
+                            hintStyle: TextStyle(color: Colors.white54),
+                            border: InputBorder.none),
+                      )),
+                  GestureDetector(
+                    onTap: () {
+                      Provider.of<FoodNotifier>(context, listen: false)
+                          .searchForFood(searchTEC.text);
+                    },
+                    child: Container(
+                        height: 40,
+                        width: 40,
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                              const Color(0x36FFFFFF),
+                              const Color(0x0FFFFFFF)
+                            ]),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Image.asset("assets/images/search_icon.png")),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
